bed0e717fca509a1e5153f37fac1315e3be25ed9
Blessing <blessing@Blessings-MacBook-Pro.local>
2021-12-12 07:03:44 +0100

    Structuring out application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+//App - contains all pointers to the app like database connection
+type App struct{}
+
+//Run - sets up our app
+func (app *App) Run() error {
+	fmt.Println("Setting up our app")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go rest API")
+
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error start App")
+		fmt.Println(err)
+	}
 }
